d282d995f16cdc7cc0fd7291bc97c602ea934fbc
Rohith P <dm.rohithp@gmail.com>
2026-07-12 01:03:06 +0530

    Update FaultHandler.cpp

--- native/FaultHandler.cpp
+++ native/FaultHandler.cpp
@@ -1,84 +1,103 @@
 #include "FaultHandler.hpp"
+
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <sys/syscall.h>
+#include <poll.h>
+#include <linux/userfaultfd.h>
 #include <cstring>
 #include <cassert>
 #include <iostream>
-#include <poll.h>
-#include <linux/userfaultfd.h>
 #include <chrono>
 
+using namespace std;
+
 FaultHandler::FaultHandler(MemoryManager* mgr)
-    : uffd(-1)
-    , running(false)
-    , zeroPage(nullptr)
-    , manager(mgr)
-    , regionBase(nullptr)
-    , regionSize(0){
-    uffd = static_cast<int>(syscall(__NR_userfaultfd, 0));  // kernel managed fault notification object
-    if (uffd == -1){
-        std::cerr << "Failed to create userfaultfd" << std::endl;
+    : uffd(-1),
+      running(false),
+      zeroPage(nullptr),
+      manager(mgr),
+      regionBase(nullptr),
+      regionSize(0){
+
+    uffd = static_cast<int>(syscall(__NR_userfaultfd, 0));
+
+    if(uffd == -1){
+        cerr << "Failed to create userfaultfd" << endl;
         exit(1);
     }
 
-    struct uffdio_api api;  // for telling the kernel you want to use the userfaultfd api
+    struct uffdio_api api;
+
     api.api = UFFD_API;
     api.features = 0;
-    if (ioctl(uffd, UFFDIO_API, &api) == -1){  
-        std::cerr << "UFFDIO_API failed" << std::endl;
+
+    if(ioctl(uffd, UFFDIO_API, &api) == -1){
+        cerr << "UFFDIO_API failed" << endl;
         exit(1);
     }
 
-    // this is a fresh uninitizalized page 
-    zeroPage = mmap(nullptr, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0); //
-    if (zeroPage == MAP_FAILED){
-        std::cerr << "Failed to allocate zero page" << std::endl;
+    zeroPage = mmap(
+        nullptr,
+        PAGE_SIZE,
+        PROT_READ | PROT_WRITE,
+        MAP_PRIVATE | MAP_ANONYMOUS,
+        -1,
+        0
+    );
+
+    if(zeroPage == MAP_FAILED){
+        cerr << "Failed to allocate zero page" << endl;
         exit(1);
     }
-    memset(zeroPage, 0, PAGE_SIZE); // creates an all zero page
+
+    memset(zeroPage, 0, PAGE_SIZE);
 }
 
 FaultHandler::~FaultHandler(){
-    if (running){
+    if(running){
         stop();
     }
-    if (zeroPage){
+
+    if(zeroPage){
         munmap(zeroPage, PAGE_SIZE);
     }
-    if (uffd != -1){
+
+    if(uffd != -1){
         close(uffd);
     }
 }
 
-// We have the usefaultd kernel object
-// now we want to register that kernel object with our virtual address space we got from mmap
 void FaultHandler::registerRegion(void* addr, size_t size){
     regionBase = addr;
     regionSize = size;
-    size_t pageCount = size/PAGE_SIZE;
-    manager->initPageTable(pageCount);  // this initializes the page table
+
+    manager->initPageTable(size / PAGE_SIZE);
 
     struct uffdio_register reg;
+
     reg.range.start = (unsigned long)addr;
     reg.range.len = size;
-    reg.mode = UFFDIO_REGISTER_MODE_MISSING; // intercept the page faults for the pages in this region
-    if (ioctl(uffd, UFFDIO_REGISTER, &reg) == -1){
-        std::cerr << "UFFDIO_REGISTER failed" << std::endl;
+    reg.mode = UFFDIO_REGISTER_MODE_MISSING;
+
+    if(ioctl(uffd, UFFDIO_REGISTER, &reg) == -1){
+        cerr << "UFFDIO_REGISTER failed" << endl;
         exit(1);
     }
+
     regions.push_back(reg);
 }
 
 void FaultHandler::start(){
     running = true;
-    handlerThread = std::thread(&FaultHandler::handleFaults, this);
+    handlerThread = thread(&FaultHandler::handleFaults, this);
 }
 
 void FaultHandler::stop(){
     running = false;
-    if (handlerThread.joinable()){
+
+    if(handlerThread.joinable()){
         handlerThread.join();
     }
 }
@@ -88,75 +107,119 @@ MemoryManager* FaultHandler::getManager(){
 }
 
 void FaultHandler::handleFaults(){
-    while (running){
-        struct pollfd pfd = {uffd, POLLIN, 0}; // creates a poll desciptor
-        int ret = poll(&pfd, 1, 1000); // waits for a page fault with a timeout of 1000ms, i.e. it checks every 1000 ms if there is a page fault
-        if (ret == -1){
-            std::cerr << "Poll failed" << std::endl;
+    while(running){
+
+        struct pollfd pfd = {uffd, POLLIN, 0};
+
+        int ret = poll(&pfd, 1, 1000);
+
+        if(ret == -1){
+            cerr << "Poll failed" << endl;
             break;
         }
-        if (ret == 0)
+
+        if(ret == 0){
             continue;
-        if (pfd.revents & POLLIN){  // userfaultfd has a message for you, probably a page fault
-            struct uffd_msg msg;
-            ssize_t nread = read(uffd, &msg, sizeof(msg));
-            if (nread == 0){
-                std::cerr << "EOF on userfaultfd" << std::endl; // userfaultfd closed, shouldnt happen, and kernel disconnects us if it does, so we just exit the loop
-                break;
-            }
-            if (nread == -1){
-                std::cerr << "Read failed" << std::endl;
-                break;
-            }
-            if (msg.event != UFFD_EVENT_PAGEFAULT){
-                std::cerr << "Unexpected event" << std::endl;
-                continue;
-            }
-
-            void* faultAddr = (void*)msg.arg.pagefault.address; //  the virtual address that caused a fault
-            // faultAddr is the address that caused the page fault, but SwapCore manages memory in terms of pages
-            // so we calculate the starting address of the page where the fault happened which is pageAddr
-            // using that we calculate the vpn
-            void* pageAddr = (void*)((unsigned long)faultAddr & ~(PAGE_SIZE - 1));
-            u64 vpn = ((u64)faultAddr - (u64)regionBase) / PAGE_SIZE;
-            assert(vpn < regionSize / PAGE_SIZE);
-
-            auto& entry = manager->pageTbl().getEntry(vpn); 
-            auto faultStart = std::chrono::high_resolution_clock::now(); // for latency
-            if (!entry.resident){
-                u32 frame = manager->allocFrame(vpn);   // allocates a frame in our frame vector
-                manager->loadPage(vpn, frame); // loads the page data into our frame from our frame vector
-                struct uffdio_copy copy;
-                copy.src = (unsigned long)manager->frameData(frame);
-                copy.dst = (unsigned long)pageAddr;
-                copy.len = PAGE_SIZE;
-                copy.mode = 0;
-                copy.copy = 0;
-                // NOW THIS IS IMPORTANT
-                // this ioctl with UFFDIO_COPY copies the data from our frame from the frame vector to an actual frame in the RAM
-                if (ioctl(uffd, UFFDIO_COPY, &copy) == -1){
-                    std::cerr << "UFFDIO_COPY failed" << std::endl;
-                    break;
-                }
-                std::cout << "Fault at VPN " << vpn << ", loaded frame " << frame << "\n";
-            } 
-            else{
-                manager->touchPage(vpn);
-                struct uffdio_copy copy;
-                copy.src = (unsigned long)manager->frameData(entry.frameIndex);
-                copy.dst = (unsigned long)pageAddr;
-                copy.len = PAGE_SIZE;
-                copy.mode = 0;
-                copy.copy = 0;
-                if (ioctl(uffd, UFFDIO_COPY, &copy) == -1){
-                    std::cerr << "UFFDIO_COPY failed" << std::endl;
-                    break;
-                }
-                std::cout << "Re-fault for resident VPN " << vpn << "\n";
-            }
-
-            auto faultEnd = std::chrono::high_resolution_clock::now(); // for latency again
-            manager->addFaultLatencyNs(std::chrono::duration_cast<std::chrono::nanoseconds>(faultEnd - faultStart).count());
         }
+
+        if(!(pfd.revents & POLLIN)){
+            continue;
+        }
+
+        struct uffd_msg msg;
+
+        ssize_t nread = read(uffd, &msg, sizeof(msg));
+
+        if(nread == 0){
+            cerr << "EOF on userfaultfd" << endl;
+            break;
+        }
+
+        if(nread == -1){
+            cerr << "Read failed" << endl;
+            break;
+        }
+
+        if(msg.event != UFFD_EVENT_PAGEFAULT){
+            cerr << "Unexpected event" << endl;
+            continue;
+        }
+
+        void* faultAddr = (void*)msg.arg.pagefault.address;
+
+        void* pageAddr =
+            (void*)((unsigned long)faultAddr & ~(PAGE_SIZE - 1));
+
+        u64 vpn =
+            ((u64)pageAddr - (u64)regionBase) / PAGE_SIZE;
+
+        assert(vpn < regionSize / PAGE_SIZE);
+
+        auto faultStart =
+            std::chrono::high_resolution_clock::now();
+
+        auto& entry = manager->pageTbl().getEntry(vpn);
+
+        if(!entry.resident){
+
+            if(!manager->hasFreeResidentSlot()){
+
+                u64 victim = manager->chooseLearnedVictim();
+
+                void* victimAddr =
+                    (char*)regionBase + victim * PAGE_SIZE;
+
+                manager->evictPage(victim);
+
+                madvise(
+                    victimAddr,
+                    PAGE_SIZE,
+                    MADV_DONTNEED
+                );
+            }
+
+            manager->loadPage(vpn);
+            struct uffdio_copy copy;
+
+            copy.src = (unsigned long)manager->bufferData(entry.bufferIndex);
+            copy.dst = (unsigned long)pageAddr;
+            copy.len = PAGE_SIZE;
+            copy.mode = 0;
+            copy.copy = 0;
+
+            if(ioctl(uffd, UFFDIO_COPY, &copy) == -1){
+                cerr << "UFFDIO_COPY failed" << endl;
+                break;
+            }
+
+            cout << "Loaded VPN " << vpn << endl;
+        }
+        else{
+
+            manager->touchPage(vpn);
+
+            struct uffdio_copy copy;
+
+            copy.src = (unsigned long)manager->bufferData(entry.bufferIndex);
+            copy.dst = (unsigned long)pageAddr;
+            copy.len = PAGE_SIZE;
+            copy.mode = 0;
+            copy.copy = 0;
+
+            if(ioctl(uffd, UFFDIO_COPY, &copy) == -1){
+                cerr << "UFFDIO_COPY failed" << endl;
+                break;
+            }
+
+            cout << "Re-fault for resident VPN " << vpn << endl;
+        }
+
+        auto faultEnd = std::chrono::high_resolution_clock::now();
+
+        manager->addFaultLatencyNs(
+            std::chrono::duration_cast<std::chrono::nanoseconds>(
+                faultEnd - faultStart
+            ).count()
+        );
     }
 }
